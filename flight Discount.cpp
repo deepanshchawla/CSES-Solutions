8c02ff4c50897d296763705456145e13d2c1146f
Deepansh Chawla <89485545+deepanshchawla@users.noreply.github.com>
2023-10-26 03:16:59 +0530

    Update flight Discount.cpp
    Explanation to the question and solution added to the code

--- flight Discount.cpp
+++ flight Discount.cpp
@@ -14,6 +14,26 @@ void init_code(){
 	#endif
 }
 
+/*
+Problem Explanation:
+
+the problem is quite simple we need to find the length of the shortest route from a source to a destination. But the catch is that there is a coupon which can be used only once and which makes the cost of the path half.
+
+Now we have to find the shortest route cost from source to destination by using atmost one coupon optimally. The test cases are quite tight so we need to think something fast and optimal
+
+Solution: if there is no flag like coupon in this question then the question is quite simple, we will simply use dijkstra to find the length of shortest route from source to destination.
+
+But here, to solve this question, i have used dijkstra in a different way. 
+	1. I have found the minimum distance from the source node to all other nodes and 
+ 	2. from the all other nodes to the destination node. Here we have inverted the graph and found the shortest distance from the destination node to all other nodes, this indirectly gives us the shortest distance from the destination to all other nodes.
+  	3. now i have traversed on each edge and tried weather can it be the part of my path with the coupon applied and will it give the shortest distance.
+   	4. this solution works because applying coupon is always better because it only reduces the path cost so we need to apply this on an edge such that by applying it on that edge it gives the overall shortest path.
+    	5. it has to be applied on on of the edge so why not try each and every edge and find the minimum distance to reach from source to destination.
+
+if you still not understood, ping me or raise an issue i will be more than happy to answer you.
+*/
+
+
 void dij(ll node, vector<ll>&a, vector<vector<pair<ll,ll>>>&mp){
 	priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>>pq;
 	pq.push({0,node});
