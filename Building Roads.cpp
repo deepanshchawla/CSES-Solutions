0c8158ef89908e4bb18e2ea1837dad35c11f20e2
Deepansh Chawla <89485545+deepanshchawla@users.noreply.github.com>
2023-10-31 02:33:13 +0530

    Update Building Roads.cpp
    explanation added to the solution

--- Building Roads.cpp
+++ Building Roads.cpp
@@ -11,6 +11,17 @@ void init_code(){
 	#endif
 }
 
+/*
+The question is quite simple we just need to find out the minimum number of roads needed to make all the cities connected.
+If it was not the case minimum the quuestion could have been done in one line as we would have connected each node to the every other node.
+
+but now in this case as we have to find the minimum number of nodes to make the cities connected so, in this solution we make the components of the connected cities. 
+1. the already connected cities will have a parent node
+2. now total_components-1 is the number of nodes which we must build to make all the cities connected.
+3. and the number of nodes we can have by connecting the parent of each successive parent node with its prev passing parent (as we have stored all the parent nodes in a vector/arr) 
+4. like if there are 4 6 8 are the parent nodes of the already connected components cities then the roads required are 4-6, 6-8 i.e 2.
+*/
+
 int findparent(int i, vector<int>&parent){
     if(parent[i]==i)return i;
     return parent[i] = findparent(parent[i],parent);
